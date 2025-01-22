44b6733c5d475a1f6bc2479b876e323d391d856d
Amine <amine.kass35@gmail.com>
2025-01-22 02:50:09 +0100

    add loop

--- src/main.c
+++ src/main.c
@@ -3,25 +3,64 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bdenfir <bdenfir@student.42.fr>            +#+  +:+       +#+        */
+/*   By: amine <amine@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 17:26:05 by bdenfir           #+#    #+#             */
-/*   Updated: 2025/01/21 19:48:33 by bdenfir          ###   ########.fr       */
+/*   Updated: 2025/01/22 02:49:47 by amine            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void prompt()
+{
+    printf("minishell> ");
+}
+
+char *read_input()
+{
+	char	*input;
+
+	input = get_next_line(STDIN_FILENO);
+	if (!input)
+	{
+		perror("get_next_line");
+		exit(EXIT_FAILURE);
+	}
+	return (input);
+}
+
 int main(int argc, char **argv, char **envp)
 {
-	t_data data;
+    t_data		data;
+	char		*input;
+	t_ast_node	*ast;
+	char		**tokens;
 
-	data.envp = copy_envp(envp);
-	char **tokens = split_whitespace(argv[argc-1]);
-	t_ast_node *ast = parse_tokens(tokens, &data);
-	print_ast(ast, "", 0);
-	exec_ast(ast, data.envp);	
-	free_args(tokens);
-	free_ast(ast);
-	free_args(data.envp);
+	if (argc != 1 || !argv[0])
+		return (0);
+    data.envp = copy_envp(envp);
+    while (1)
+    {
+        prompt();
+        input = read_input();
+        if (!input)
+            break ;
+        tokens = split_whitespace(input);
+        if (!tokens)
+        {
+            free(input);
+            continue ;
+        }
+        ast = parse_tokens(tokens, &data);
+        if (ast)
+        {
+            exec_ast(ast, data.envp);
+            free_ast(ast);
+        }
+        free_tokens(tokens);
+        free(input);
+    }
+    free_args(data.envp);
+    return 0;
 }
