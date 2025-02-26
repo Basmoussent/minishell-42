d2b69e5a1101fb8802ac58316f05da03e9b301d6
Amine <amine.kass35@gmail.com>
2025-02-26 23:53:51 +0100

    fixing pushes

--- src/lexer/more_utils.c
+++ src/lexer/more_utils.c
@@ -6,7 +6,7 @@
 /*   By: amine <amine@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 22:44:55 by amine             #+#    #+#             */
-/*   Updated: 2025/02/26 22:57:25 by amine            ###   ########.fr       */
+/*   Updated: 2025/02/26 23:44:36 by amine            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,49 @@ int		found_builtin(char *input)
 	return (input[0] == '|' || input[0] == '>' || input[0] == '<');
 }
 
-char	**fix_order(char **tokens)
+static char	**temp_loop(char **tokens, char **fix_tokens)
 {
-    int		i;
-    char	**fixed_tokens;
+	int		i;
 
-    fixed_tokens = malloc(sizeof(char *) * (count_tokens_array(tokens) + 1));
-    if (!fixed_tokens)
-        return (NULL);
-    i = 0;
+	i = 0;
     while (tokens[i])
     {
-        if (is_speci(tokens[i]) && !is_speci(tokens[i + 1])
+        if (tokens[i] && tokens[i + 1] && tokens[i + 2] && 
+			is_speci(tokens[i]) && !is_speci(tokens[i + 1])
 			&& !is_speci(tokens[i + 2]))
 		{
-			fixed_tokens[i + 1] = ft_strdup(tokens[i]);
-			fixed_tokens[i] = ft_strdup(tokens[i + 2]);
-			fixed_tokens[i + 2] = ft_strdup(tokens[i + 1]);
+			fix_tokens[i] = ft_strdup(tokens[i + 2]);
+			fix_tokens[i + 1] = ft_strdup(tokens[i]);
+			fix_tokens[i + 2] = ft_strdup(tokens[i + 1]);
+			if (!fix_tokens[i] || !fix_tokens[i + 1] || !fix_tokens[i + 2])
+				return (NULL);
 			i += 3;
 		}
 		else
 		{
-			fixed_tokens[i] = ft_strdup(tokens[i]);
+			fix_tokens[i] = ft_strdup(tokens[i]);
+			if (!fix_tokens[i])
+				return (NULL);
 			i++;
 		}
     }
-    fixed_tokens[i] = NULL;
-	printf("%s %s %s\n", fixed_tokens[0], fixed_tokens[1], fixed_tokens[2]);
+	return (fix_tokens);
+}
+
+char	**fix_order(char **tokens)
+{
+    char	**fixed_tokens;
+	int		count;
+
+	count = count_tokens_array(tokens);
+    fixed_tokens = malloc(sizeof(char *) * (count_tokens_array(tokens) + 1));
+    if (!fixed_tokens)
+	{
+        return (NULL);
+	}
+	fixed_tokens = temp_loop(tokens, fixed_tokens);
+	if (!fixed_tokens)
+		return (NULL);
+    fixed_tokens[count] = NULL;
     return (fixed_tokens);
 }
